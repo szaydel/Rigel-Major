e2fb0731e2169360a25702c59b9123c07f8773ba
Sam Zaydel <szaydel@gmail.com>
2010-06-05 15:17:03 -0700

    Made changes to daily-cron-script

--- cron-scripts/backup-def-01.sh
+++ cron-scripts/backup-def-01.sh
@@ -1,6 +1,43 @@
 #!/bin/bash
 # Run backintime as szaydel daily
+
+###############################################################################
+### Step 1 - Set Variables and Create functions
+###############################################################################
+
+RET_CODE=$?
 BKUPDIR=/export/nfs/backups
+LOGSDIR=${BKUPDIR}/logs
+MYDATE=$(date +%Y%m%d)
+ERRLOG=${LOGSDIR}/error-${MYDATE}.log
+INFOLOG=${LOGSDIR}/info-${MYDATE}.log
+DEBUG=1
+
+# Functions for error control
+alert () 
+{
+# usage: alert <$?> <object>
+if [ "$1" -ne 0 ]; then
+   echo "WARNING: $2 did not complete successfully." >&2 && logger -p cron.notice "WARNING: $2 did not complete successfully."
+   exit $1
+   else
+   echo "INFO: $2 completed successfully." >&2 && logger -p cron.notice "INFO: $2 completed successfully."
+fi
+}
+
+###############################################################################
+### Step 2 - Mount NFS and Run Backup
+###############################################################################
+
+## Mount directory to NFS storage
 [ ! -d ${BKUPDIR}/${USER} ] && mount ${BKUPDIR} # Need to make sure that we have /export/nfs/backups mounted
-nice -n 19 /usr/bin/backintime --backup-job && logger -p cron.notice "backintime: Backup job for `whoami` finished successfully."
+
+## Execute backup job and report on its success or failure
+[ ${DEBUG} -eq 0 ] && nice -n 19 /usr/bin/backintime --backup-job 2> /dev/null
+[ ${DEBUG} -eq 1 ] && nice -n 19 /usr/bin/backintime --backup-job 2> ${ERRLOG}
+[ ${DEBUG} -eq 2 ] && nice -n 19 /usr/bin/backintime --backup-job > ${INFOLOG} 2> ${ERRLOG}
+
+alert ${RET_CODE} "backintime: Backup job for `whoami`"
+
+# logger -p cron.notice "backintime: Backup job for `whoami` finished successfully."
 umount ${BKUPDIR} # Once we are done, we will umount NFS mount
