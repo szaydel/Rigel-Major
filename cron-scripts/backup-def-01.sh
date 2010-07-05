6880e2bbf76a8446b8f97e85be28587b29a7cd36
Sam Zaydel <szaydel@gmail.com>
2010-07-04 18:45:56 -0700

    Corrected logic of the '&&' to make sure that correct RET_CODE was being set. Added function to clean-up logs older than 30 days.

--- cron-scripts/backup-def-01.sh
+++ cron-scripts/backup-def-01.sh
@@ -25,6 +25,12 @@ if [ "$1" -ne 0 ]; then
 fi
 }
 
+remove_logs ()
+{
+local FIND_CMD=/usr/bin/find
+"${FIND_CMD}" "${LOGSDIR}" -depth -name "[err|info]*" -mtime +30 -delete
+}
+
 ###############################################################################
 ### Step 2 - Mount NFS and Run Backup
 ###############################################################################
@@ -33,11 +39,19 @@ fi
 [ ! -d ${BKUPDIR}/${USER} ] && mount ${BKUPDIR} # Need to make sure that we have /export/nfs/backups mounted
 
 ## Execute backup job and report on its success or failure
-[ ${DEBUG} -eq 0 ] && nice -n 19 /usr/bin/backintime --backup-job 2> /dev/null
-[ ${DEBUG} -eq 1 ] && nice -n 19 /usr/bin/backintime --backup-job 2> ${ERRLOG}
-[ ${DEBUG} -eq 2 ] && nice -n 19 /usr/bin/backintime --backup-job > ${INFOLOG} 2> ${ERRLOG}
+[ ${DEBUG} -eq 0 ] && (nice -n 19 /usr/bin/backintime --backup-job 2> /dev/null; RET_CODE=$?)
+[ ${DEBUG} -eq 1 ] && (nice -n 19 /usr/bin/backintime --backup-job 2> ${ERRLOG}; RET_CODE=$?)
+[ ${DEBUG} -eq 2 ] && (nice -n 19 /usr/bin/backintime --backup-job > ${INFOLOG} 2> ${ERRLOG}; RET_CODE=$?)
 
 alert ${RET_CODE} "backintime: Backup job for `whoami`"
+
+###############################################################################
+### Step 3 - Clean-up and unmount mounted filesystem
+###############################################################################
+[ ${DEBUG} -eq 0 ] && (remove_logs 2> /dev/null; RET_CODE=$?)
+[ ${DEBUG} -eq 1 ] && (remove_logs 2> ${ERRLOG}; RET_CODE=$?)
+[ ${DEBUG} -eq 2 ] && (remove_logs > ${INFOLOG} 2> ${ERRLOG}; RET_CODE=$?)
+alert ${RET_CODE} "backintime: Removal of logs for `whoami`"
 
 # logger -p cron.notice "backintime: Backup job for `whoami` finished successfully."
 umount ${BKUPDIR} # Once we are done, we will umount NFS mount
