c4d526d5908fa45080df987bc181eb910e6e1177
Sam Zaydel <szaydel@gmail.com>
2010-08-08 17:07:43 -0700

    Added function to check for another instance of self already running.

--- cron-scripts/backup-def-01.sh
+++ cron-scripts/backup-def-01.sh
@@ -9,6 +9,13 @@ RET_CODE=$?
 BKUPDIR=/export/nfs/backups
 LOGSDIR=${BKUPDIR}/logs
 MYDATE=$(date +%Y%m%d)
+RUN_AS=$(whoami)
+
+## Used in determining if another instance is running
+MYNAME=$(basename $0)
+MYPID=$$
+
+## Log files and Debug level
 ERRLOG=${LOGSDIR}/error-${MYDATE}.log
 INFOLOG=${LOGSDIR}/info-${MYDATE}.log
 DEBUG=1
@@ -31,6 +38,19 @@ local FIND_CMD=/usr/bin/find
 "${FIND_CMD}" "${LOGSDIR}" -depth -name "[err|info]*" -mtime +30 -delete
 }
 
+check_for_another_instance ()
+{
+pgrep "${MYNAME}" | grep -v "${MYPID}" &> /dev/null; RET_CODE=$?
+    if [[ "${RET_CODE}" = "0" ]]; then
+            return 1
+        else
+            return 0    
+    fi
+}
+
+## If another instance of this script already running 
+check_for_another_instance || exit 1
+
 ###############################################################################
 ### Step 2 - Mount NFS and Run Backup
 ###############################################################################
@@ -43,15 +63,22 @@ local FIND_CMD=/usr/bin/find
 [ ${DEBUG} -eq 1 ] && (nice -n 19 /usr/bin/backintime --backup-job 2> ${ERRLOG}; RET_CODE=$?)
 [ ${DEBUG} -eq 2 ] && (nice -n 19 /usr/bin/backintime --backup-job > ${INFOLOG} 2> ${ERRLOG}; RET_CODE=$?)
 
-alert ${RET_CODE} "backintime: Backup job for `whoami`"
+alert ${RET_CODE} "backintime: Backup job for ${RUN_AS}"
 
 ###############################################################################
 ### Step 3 - Clean-up and unmount mounted filesystem
 ###############################################################################
+
+## At this point we want to clean-up the logs
 [ ${DEBUG} -eq 0 ] && (remove_logs 2> /dev/null; RET_CODE=$?)
 [ ${DEBUG} -eq 1 ] && (remove_logs 2> ${ERRLOG}; RET_CODE=$?)
 [ ${DEBUG} -eq 2 ] && (remove_logs > ${INFOLOG} 2> ${ERRLOG}; RET_CODE=$?)
-alert ${RET_CODE} "backintime: Removal of logs for `whoami`"
+alert ${RET_CODE} "backintime: Removal of logs for ${RUN_AS}"
 
-# logger -p cron.notice "backintime: Backup job for `whoami` finished successfully."
-umount ${BKUPDIR} # Once we are done, we will umount NFS mount
+# logger -p cron.notice "backintime: Backup job for "${RUN_AS}" finished successfully."
+
+# Once we are done, we will umount NFS mount
+while [[ -d ${BKUPDIR}/${USER} ]]
+    do
+        umount "${BKUPDIR}"
+    done
